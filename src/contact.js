05544b80bfc3ba7bcffaa8f433403c79e93ff441
Boris Yankov <borisyankov@gmail.com>
2014-11-19 09:14:18 +0200

    Add slick form style

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,7 @@
         message: 'Contact Us',
         position: 'right',
         textColor: 'white',
-        backgroundColor: 'purple'
+        backgroundColor: '#11a9cc'
     };
 
     var style = 'position: fixed; z-index: 100000001; height: 40px; line-height: 40px; padding: 0 15px; ' +
@@ -12,7 +12,7 @@
             ? 'bottom: 0; right: 25%;'
             : 'right: 40px; transform: rotate(-90deg); transform-origin: 100% 0; top: 25%;') +
         'cursor: pointer; border-radius: 5px 5px 0 0;' +
-        'color: white; background-color: #48c019';
+        'color: white; background-color: #11a9cc';
 
     var addHtml = '<div style="' + style + '">CONTACT US</div>';
 
